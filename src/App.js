39dfb1b2263a2b041c720789ccd5ef50f40f866e
Phuoc Thanh <megastar24577@gmail.com>
2021-10-07 14:48:21 +0700

    New Feature ABAF - Approval

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/Header/navBar";
+import Dashboard from "./pages/AdminDashboard/Dashboard";
 import Approval from "./pages/Approval/components/Approval";
 import HomePage from "./pages/Newest/HomePage";
-import BlogContentFeature from "./pages/Showblogcontent/BlogContent";
-import Dashboard from "./pages/AdminDashboard/Dashboard";
 import PostBlog from "./pages/PostBlog/PostBlog";
+import BlogContentFeature from "./pages/Showblogcontent/BlogContent";
 function App() {
   return (
     <div className="App font-monsterrat">
@@ -15,8 +15,8 @@ function App() {
         <div>
           <Switch>
             <Route path="/" component={HomePage} exact />
-            <Route path= '/blogdetail' component = {BlogContentFeature} exact/>
-            <Route path='/dashboard' component = {Dashboard} exact/>
+            <Route path="/blogdetail" component={BlogContentFeature} exact />
+            <Route path="/dashboard" component={Dashboard} exact />
             <Route path="/createNewPost" component={PostBlog} />
             <Route path="/approval" component={Approval} />
           </Switch>
