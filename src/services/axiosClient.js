ae530c274284565ca42f47c135530c393aca1b57
Dong Quan <dongquan1212@gmail.com>
2021-11-24 13:08:57 +0700

    axios

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://5aa5-2402-800-6314-f05d-1df1-f513-4880-e1c.ngrok.io/fptu-blog",
+  // baseURL: "http://5aa5-2402-800-6314-f05d-1df1-f513-4880-e1c.ngrok.io/fptu-blog",
+  baseURL: "https://34.124.170.202/fptu-blog",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
   },
